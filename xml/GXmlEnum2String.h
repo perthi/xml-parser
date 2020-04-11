f3ec9a4a75b21492fa92d50cbaa8b71d709f27cc
Per Thomas <you@example.com>
2020-04-11 22:02:44 +0200

    refactoring

---
+++ xml/GXmlEnum2String.h
@@ -0,0 +1,20 @@
+// -*- mode: c++ -*-
+#ifndef GXMLENUM2STRING_H
+#define GXMLENUM2STRING_H
+
+
+#include <xml/GXmlEnums.h>
+#include <string>
+
+using std::string;
+
+
+class  GXmlEnum2String
+{
+	public:
+	   GXmlEnum2String();
+	   virtual ~GXmlEnum2String();
+	   static string  Enum2String( eXML_NODETYPE );
+};
+
+#endif
