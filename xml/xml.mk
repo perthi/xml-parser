ebd20ed875abb9e3c54740cc861a461d2f13b10e
Per Thomas <you@example.com>
2020-04-27 20:31:08 +0200

    added module file that was deleted by mistake

---
+++ xml/xml.mk
@@ -0,0 +1,35 @@
+
+
+
+## ifneq (ARM, $(TARGET))
+LIBNAME_SO:=libxml-embc.so	
+## endif	
+
+#LIBNAME_A:=libxml-embc.a
+
+
+SRCCPP+= GXmlAttribute.cpp \
+	GXmlClassFactory.cpp \
+	GXmlDocTypeNode.cpp \
+	GXmlProcInstructNode.cpp \
+	GXmlStreamReaderSp.cpp \
+	GXmlTagNode.cpp \
+	GXmlValidator.cpp \
+	GXmlCDataNode.cpp \
+	GXmlCommentNode.cpp \
+	GXmlNodeImpl.cpp \
+	GXmlStreamReaderImpl.cpp \
+	GXmlStreamWriterImpl.cpp \
+	GXmlTextNode.cpp \
+	GXmlValidatorImpl.cpp \
+	GTextXml.cpp \
+	GPrintable.cpp \
+	GCommonXML.cpp \
+	GXmlParser.cpp \
+	GString2Number.cpp \
+	GXmlEntity.cpp \
+	GXmlEnum2String.cpp
+
+
+INCLUDES+=$(XML_INCLUDES)
+include ../../common.mk
